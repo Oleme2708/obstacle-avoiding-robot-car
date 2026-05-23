3b576ed633103a5b3b8b79dcae324815394097e6
Oleme <xeniel2708@gmail.com>
2026-05-23 17:42:26 +1000

    finishing code

--- src/main.cpp
+++ src/main.cpp
@@ -6,52 +6,123 @@
 // sonar sensor setup
 #define SONAR_NUM 1
 #define MAX_DISTANCE 200
-
-// state setup
-enum state
-{
-    Forward,
-    Backward,
-    TurnLeft,
-    TurnRight
-}
-
+#define trig_pin 12
+#define echo_pin 11
 NewPing Sonar[SONAR_NUM] = {
-    NewPing(4, 5, MAX_DISTANCE), // trigger pin, echo pin, max distance
-}
-
+    NewPing(trig_pin, echo_pin, MAX_DISTANCE), // trigger pin, echo pin, max distance
+};
+// motors setup
+// motor A connections
+int ENA = 9;
+int IN1 = 2;
+int IN2 = 3;
+// motor B connections
+int ENB = 10;
+int IN3 = 4;
+int IN4 = 5;
+// Servo setup
+Servo myservo;
+int servopin = 6;
+// state setup
+enum State
+{
+    STATE_Forward,
+    STATE_Backward,
+    STATE_Scan,
+    STATE_TurnLeft,
+    STATE_TurnRight
+};
+State currentState = STATE_Forward;
+unsigned long stateStartTime = 0;
+// initial values
+int distance = 0;
+int distance_left = 0;
+int distance_right = 0;
+// Declare user_defined function
+void MoveForward();
+void MoveBackward();
+void TurnLeft();
+void TurnRight();
+void Scan();
+void Stop();
+void ChangeState(State newState);
+int LookLeft();
+int LookRight();
+int readPing();
+//
 void setup()
 {
     Serial.begin(9600);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    // setup max speed (change if 1 motor is faster than other)
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    myservo.attach(servopin);
+    myservo.write(115);
+    // reading distance values 4 times
+    int total = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        total += readPing();
+        delay(100);
+    }
+    distance = total / 4;
+    stateStartTime = millis();
 }
 
 void loop()
 {
+    // inital distance setup
+    distance = readPing();
+    Serial.println(distance);
     // changing state
-    switch (state)
+    switch (currentState)
     {
-    case Forward:
+    case STATE_Forward:
         MoveForward();
         if (distance <= 20)
         {
             Stop();
-            ChangeState(Backward);
+            ChangeState(STATE_Backward);
         }
         break;
-    case Backward:
+    case STATE_Backward:
         MoveBackward();
         if (millis() - stateStartTime >= 500)
         {
             Stop();
-            ChangeState(Scan);
+            ChangeState(STATE_Scan);
         }
         break;
-    case scan:
+    case STATE_Scan:
         Scan();
+        if (distance_right >= distance_left)
+        {
+            ChangeState(STATE_TurnRight);
+        }
+        else
+        {
+            ChangeState(STATE_TurnLeft);
+        }
+        break;
+    case STATE_TurnRight:
         if (millis() - stateStartTime >= 500)
         {
             Stop();
-            ChangeState(Scan);
+            ChangeState(STATE_Forward);
+        }
+        break;
+    case STATE_TurnLeft:
+        TurnLeft();
+        if (millis() - stateStartTime >= 500)
+        {
+            Stop();
+            ChangeState(STATE_Forward);
         }
         break;
     }
@@ -60,38 +131,94 @@ void loop()
 // user-deifned function
 void MoveForward()
 {
+    // motor A forward
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    // motor B forward
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
 }
 
-void Backward()
+void MoveBackward()
 {
+    // motor A backward
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    // motor B backward
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
 }
 
 void TurnLeft()
 {
+    // motor A forward
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    // motor B stop
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
 }
 
 void TurnRight()
 {
-}
-
-void Scan()
-{
+    // motor A stop
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    // motor B forward
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
 }
 
 void Stop()
 {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
 }
 
-void ChangeState(State newstate)
+void ChangeState(State newState)
 {
-    state = newstate;
+    currentState = newState;
     stateStartTime = millis();
 }
 
-void LookLeft()
+void Scan()
 {
+    Stop();
+    distance_left = LookLeft();
+    delay(200);
+    distance_right = LookRight();
+    delay(200);
+    myservo.write(115);
+    delay(200);
+    Serial.print("left: ");
+    Serial.println(distance_left);
+    Serial.print("right: ");
+    Serial.println(distance_right);
 }
 
-void LookRight()
+int LookLeft()
 {
+    myservo.write(50);
+    delay(500);
+    return readPing();
+}
+
+int LookRight()
+{
+    myservo.write(170);
+    delay(500);
+    return readPing();
+}
+
+int readPing()
+{
+    delay(50);
+    int cm = Sonar[0].ping_cm();
+    if (cm == 0)
+    {
+        cm = MAX_DISTANCE;
+    }
+    return cm;
 }
